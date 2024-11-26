8ba45e789c17ff3f0aac6cf897dcc68d762bba57
Sys Admin <s.admin@dtoffice.local>
2024-11-26 10:12:47 +0200

    second

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v11.3.3',
-        'version' => '11.3.3.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4c5ccdb2cf532571314155dc8234014b69914a00',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v11.3.3',
-            'version' => '11.3.3.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4c5ccdb2cf532571314155dc8234014b69914a00',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
